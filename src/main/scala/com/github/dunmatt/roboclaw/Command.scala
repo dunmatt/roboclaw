16b062457764dd4a65e93286d0b8020e8339e494
M@ <mattdunlap+github@gmail.com>
2016-01-03 21:23:36 -0500

    refactored the commands to use ByteBuffer instead of Seq[Byte]

--- src/main/scala/com/github/dunmatt/roboclaw/Command.scala
+++ src/main/scala/com/github/dunmatt/roboclaw/Command.scala
@@ -1,5 +1,6 @@
 package com.github.dunmatt.roboclaw
 
+import java.nio.ByteBuffer
 import scala.concurrent.Promise
 import squants.electro.{ ElectricCurrent, ElectricPotential }
 import squants.electro.ElectricCurrentConversions._
@@ -9,211 +10,225 @@ import squants.thermal.TemperatureConversions._
 
 sealed trait Command {
   type ResponseType
-  val resultsPromise = Promise[ResponseType]
+  protected[roboclaw] val resultsPromise = Promise[ResponseType]
   val results = resultsPromise.future
-  def bytes: Seq[Byte]
-  def parseResults(data: Seq[Byte]): ResponseType
+
+  def address: Byte
+  def command: Byte
+  def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    2
+  }
+  def parseResults(data: ByteBuffer): ResponseType
+
+  def addCrc(buf: ByteBuffer, packetLenth: Int): Unit = {
+    buf.putChar(packetLenth, Utilities.crc16(buf, packetLenth))
+  }
 }
+
+sealed trait UnitCommand extends Command {
+  type ResponseType = Unit
+  def parseResults(data: ByteBuffer) = Unit  // TODO: barf everywhere if we don't get 0xff
+}
+
 
 // COMPATIBILITY COMMANDS
-case class DriveForwardM1(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x00, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+sealed trait SimpleMotorCommand extends UnitCommand {
+  def speed: Byte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.put(2, speed)
+    addCrc(buf, 3)
+    5
+  }
 }
 
-case class DriveBackwardsM1(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x01, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveForwardM1(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 0.toByte
 }
 
-case class DriveForwardM2(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x04, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveBackwardsM1(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 1.toByte
 }
 
-case class DriveBackwardsM2(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x05, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveForwardM2(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 4.toByte
 }
 
-case class DriveM1(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x06, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveBackwardsM2(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 5.toByte
 }
 
-case class DriveM2(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x07, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveM1(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 6.toByte
+}
+
+case class DriveM2(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 7.toByte
 }
 
 
 // MIXED MODE (ie diff dive) COMPATIBILITY COMMANDS
 // NOTE: these commands only work after the controller has recieved one linear and
 //       one rotational command, make sure to chain them up
-case class DriveForward(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x08, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveForward(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 8.toByte
 }
 
-case class DriveBackwards(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x09, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveBackwards(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 9.toByte
 }
 
-case class TurnRight(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x0A, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class TurnRight(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 10.toByte
 }
 
-case class TurnLeft(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x0B, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class TurnLeft(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 11.toByte
 }
 
-case class DriveForwardOrBackward(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x0C, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class DriveForwardOrBackward(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 12.toByte
 }
 
-case class TurnLeftOrRight(address: Byte, speed: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x0D, speed))
-  def parseResults(data: Seq[Byte]) = Unit
+case class TurnLeftOrRight(address: Byte, speed: Byte) extends SimpleMotorCommand {
+  val command = 13.toByte
 }
 
 
 // VERSION, STATUS, AND SETTINGS COMMANDS
 case class ReadFirmwareVersion(address: Byte) extends Command {
   type ResponseType = String
-  def bytes = Seq(address, 0x15)
-  def parseResults(data: Seq[Byte]) = {
-    new String(data.slice(0, data.indexOfSlice(Seq(0x0A, 0x00))).toArray)
+  val command = 21.toByte
+  def parseResults(data: ByteBuffer) = {
+    val arr = data.array
+    new String(arr.slice(data.arrayOffset, arr.indexOfSlice(Seq(0x0A, 0x00))))
   }
 }
 
 case class ReadMainBatteryVoltage(address: Byte) extends Command {
   type ResponseType = ElectricPotential
-  def bytes = Seq(address, 0x18)
-  def parseResults(data: Seq[Byte]) = {
-    Utilities.readSignedShort(data).volts / 10d
-  }
+  val command = 24.toByte
+  def parseResults(data: ByteBuffer) = data.getShort(0).volts / 10d
 }
 
 case class ReadLogicBatteryVoltage(address: Byte) extends Command {
   type ResponseType = ElectricPotential
-  def bytes = Seq(address, 0x19)
-  def parseResults(data: Seq[Byte]) = {
-    Utilities.readSignedShort(data).volts / 10d
-  }
+  val command = 25.toByte
+  def parseResults(data: ByteBuffer) = data.getShort(0).volts / 10d
 }
 
 case class ReadMotorPwmValues(address: Byte) extends Command {
   type ResponseType = TwoMotorData[Double]
-  def bytes = Seq(address, 0x30)
-  def parseResults(data: Seq[Byte]) = {
-    TwoMotorData( Utilities.readSignedShort(data) / 32767d
-                , Utilities.readSignedShort(data.drop(2)) / 32767d)
+  val command = 48.toByte
+  def parseResults(data: ByteBuffer) = {
+    TwoMotorData( data.getShort(0) / 32767d
+                , data.getShort(2) / 32767d)
   }
 }
 
 case class ReadMotorCurrents(address: Byte) extends Command {
   type ResponseType = TwoMotorData[ElectricCurrent]
-  def bytes = Seq(address, 0x31)
-  def parseResults(data: Seq[Byte]) = {
-    TwoMotorData( Utilities.readSignedShort(data).amps / 100d
-                , Utilities.readSignedShort(data.drop(2)).amps / 100d)
+  val command = 49.toByte
+  def parseResults(data: ByteBuffer) = {
+    TwoMotorData( data.getShort(0).amps / 100d
+                , data.getShort(2).amps / 100d)
   }
 }
 
 case class SetMainBatteryVoltages( address: Byte
                                  , min: ElectricPotential
-                                 , max: ElectricPotential) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc( Seq[Byte](address, 0x39)
-                               ++ Utilities.writeSignedShort(min.toVolts * 10)
-                               ++ Utilities.writeSignedShort(max.toVolts * 10))
-  def parseResults(data: Seq[Byte]) = Unit
+                                 , max: ElectricPotential) extends UnitCommand {
+  val command = 57.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.putShort(2, (min.toVolts * 10).toShort)
+    buf.putShort(4, (max.toVolts * 10).toShort)
+    addCrc(buf, 6)
+    8
+  }
 }
 
 case class SetLogicBatteryVoltages( address: Byte
                                   , min: ElectricPotential
-                                  , max: ElectricPotential) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc( Seq[Byte](address, 0x3A)
-                               ++ Utilities.writeSignedShort(min.toVolts * 10)
-                               ++ Utilities.writeSignedShort(max.toVolts * 10))
-  def parseResults(data: Seq[Byte]) = Unit
+                                  , max: ElectricPotential) extends UnitCommand {
+  val command = 58.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.putShort(2, (min.toVolts * 10).toShort)
+    buf.putShort(4, (max.toVolts * 10).toShort)
+    addCrc(buf, 6)
+    8
+  }
 }
 
 case class ReadMainBatteryVoltageSettings(address: Byte) extends Command {
   type ResponseType = Range[ElectricPotential]
-  def bytes = Seq(address, 0x3B)
-  def parseResults(data: Seq[Byte]) = {
-    Range( Utilities.readSignedShort(data).volts / 10d
-         , Utilities.readSignedShort(data.drop(2)).volts / 10d)
+  val command = 59.toByte
+  def parseResults(data: ByteBuffer) = {
+    Range( data.getShort(0).volts / 10d
+         , data.getShort(2).volts / 10d)
   }
 }
 
 case class ReadLogicBatteryVoltageSettings(address: Byte) extends Command {
   type ResponseType = Range[ElectricPotential]
-  def bytes = Seq(address, 0x3C)
-  def parseResults(data: Seq[Byte]) = {
-    Range( Utilities.readSignedShort(data).volts / 10d
-         , Utilities.readSignedShort(data.drop(2)).volts / 10d)
+  val command = 60.toByte
+  def parseResults(data: ByteBuffer) = {
+    Range( data.getShort(0).volts / 10d
+         , data.getShort(2).volts / 10d)
   }
 }
 
 case class SetS3S4S5Modes( address: Byte
                          , s3: Byte
                          , s4: Byte
-                         , s5: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq[Byte](address, 0x4A, s3, s4, s5))
-  def parseResults(data: Seq[Byte]) = Unit
+                         , s5: Byte) extends UnitCommand {
+  val command = 74.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.put(2, s3)
+    buf.put(3, s4)
+    buf.put(4, s5)
+    addCrc(buf, 5)
+    7
+  }
 }
 
 case class GetS3S4S5Modes(address: Byte) extends Command {
   type ResponseType = (Byte, Byte, Byte)
-  def bytes = Seq[Byte](address, 0x4B)
-  def parseResults(data: Seq[Byte]) = (data(0), data(1), data(2))
+  val command = 75.toByte
+  def parseResults(data: ByteBuffer) = (data.get(0), data.get(1), data.get(2))
 }
 
 object SPinModes {
-  val DISABLED = 0x0.toByte
+  val DISABLED        = 0x0.toByte
   val E_STOP_LATCHING = 0x1.toByte
-  val E_STOP = 0x2.toByte
-  val VOLTAGE_CLAMP = 0x3.toByte
-  val M1_HOME = 0x4.toByte
-  val M2_HOME = 0x4.toByte
+  val E_STOP          = 0x2.toByte
+  val VOLTAGE_CLAMP   = 0x3.toByte
+  val M1_HOME         = 0x4.toByte
+  val M2_HOME         = 0x4.toByte
 }
   
-case class FactoryReset(address: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Seq(address, 0x50)
-  def parseResults(data: Seq[Byte]) = Unit
+case class FactoryReset(address: Byte) extends UnitCommand {
+  val command = 80.toByte
 }
 
 case class ReadPrimaryTemperature(address: Byte) extends Command {
   type ResponseType = Temperature
-  def bytes = Seq(address, 0x52)
-  def parseResults(data: Seq[Byte]) = Utilities.readSignedShort(data).celsius / 10d
+  val command = 82.toByte
+  def parseResults(data: ByteBuffer) = data.getShort(0).celsius / 10d
 }
 
 case class ReadSecondaryTemperature(address: Byte) extends Command {
   type ResponseType = Temperature
-  def bytes = Seq(address, 0x53)
-  def parseResults(data: Seq[Byte]) = Utilities.readSignedShort(data).celsius / 10d
+  val command = 83.toByte
+  def parseResults(data: ByteBuffer) = data.getShort(0).celsius / 10d
 }
 
 object StatusFlags {
@@ -259,8 +274,8 @@ case class RoboclawStatus(status: Short) {
 
 case class ReadStatus(address: Byte) extends Command {
   type ResponseType = RoboclawStatus
-  def bytes = Seq(address, 0x5A)
-  def parseResults(data: Seq[Byte]) = RoboclawStatus(Utilities.readSignedShort(data))
+  val command = 90.toByte
+  def parseResults(data: ByteBuffer) = RoboclawStatus(data.getShort(0))
 }
 
 sealed trait EncoderMode
@@ -269,80 +284,119 @@ case object ABSOLUTE extends EncoderMode
 
 case class ReadEncoderMode(address: Byte) extends Command {
   type ResponseType = TwoMotorData[EncoderMode]
-  def bytes = Seq(address, 0x5B)
-  def parseResults(data: Seq[Byte]) = TwoMotorData( if((data(0) & 1) == 0) QUADRATURE else ABSOLUTE
-                                                  , if((data(1) & 1) == 0) QUADRATURE else ABSOLUTE)
+  val command = 91.toByte
+  def parseResults(data: ByteBuffer) = TwoMotorData( if((data.get(0) & 1) == 0) QUADRATURE else ABSOLUTE
+                                                   , if((data.get(1) & 1) == 0) QUADRATURE else ABSOLUTE)
 }
 
-case class SetMotor1EncoderMode(address: Byte, mode: EncoderMode) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x5C, if(mode == ABSOLUTE) 1 else 0))
-  def parseResults(data: Seq[Byte]) = Unit
+case class SetMotor1EncoderMode(address: Byte, mode: EncoderMode) extends UnitCommand {
+  val command = 92.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.put(2, if(mode == ABSOLUTE) 1 else 0)
+    addCrc(buf, 3)
+    5
+  }
 }
 
-case class SetMotor2EncoderMode(address: Byte, mode: EncoderMode) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x5D, if(mode == ABSOLUTE) 1 else 0))
-  def parseResults(data: Seq[Byte]) = Unit
+case class SetMotor2EncoderMode(address: Byte, mode: EncoderMode) extends UnitCommand {
+  val command = 93.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.put(2, if(mode == ABSOLUTE) 1 else 0)
+    addCrc(buf, 3)
+    5
+  }
 }
 
-case class WriteSettingsToEeprom(address: Byte) extends Command {
-  type ResponseType = Unit
-  def bytes = Seq(address, 0x5E)
-  def parseResults(data: Seq[Byte]) = Unit
+case class WriteSettingsToEeprom(address: Byte) extends UnitCommand {
+  val command = 94.toByte
 }
 
 case class ReadSettingsFromEeprom(address: Byte) extends Command {
   type ResponseType = TwoMotorData[EncoderMode]
-  def bytes = Seq(address, 0x5F)
-  def parseResults(data: Seq[Byte]) = TwoMotorData( if((data(0) & 1) == 0) QUADRATURE else ABSOLUTE
-                                                  , if((data(1) & 1) == 0) QUADRATURE else ABSOLUTE)
+  val command = 95.toByte
+  def parseResults(data: ByteBuffer) = TwoMotorData( if((data.get(0) & 1) == 0) QUADRATURE else ABSOLUTE
+                                                   , if((data.get(1) & 1) == 0) QUADRATURE else ABSOLUTE)
 }
 
-case class SetM1CurrentLimit(address: Byte, max: ElectricCurrent) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc( Seq(address, 0x86.toByte)
-                               ++ Utilities.writeSignedShort(max.toAmperes * 100)
-                               ++ Seq[Byte](0, 0))
-  def parseResults(data: Seq[Byte]) = Unit
+case class SetM1CurrentLimit(address: Byte, max: ElectricCurrent) extends UnitCommand {
+  val command = 134.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.putShort(2, (max.toAmperes * 100).toShort)
+    buf.putShort(4, 0)
+    addCrc(buf, 6)
+    8
+  }
 }
 
-case class SetM2CurrentLimit(address: Byte, max: ElectricCurrent) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc( Seq(address, 0x87.toByte)
-                               ++ Utilities.writeSignedShort(max.toAmperes * 100)
-                               ++ Seq[Byte](0, 0))
-  def parseResults(data: Seq[Byte]) = Unit
+case class SetM2CurrentLimit(address: Byte, max: ElectricCurrent) extends UnitCommand {
+  val command = 135.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.putShort(2, (max.toAmperes * 100).toShort)
+    buf.putShort(4, 0)
+    addCrc(buf, 6)
+    8
+  }
 }
 
 case class ReadM1CurrentLimit(address: Byte) extends Command {
   type ResponseType = Range[ElectricCurrent]
-  def bytes = Seq(address, 0x88.toByte)
-  def parseResults(data: Seq[Byte]) = Range( Utilities.readSignedShort(data.drop(2)).amps / 100
-                                           , Utilities.readSignedShort(data).amps / 100)
+  val command = 136.toByte
+  def parseResults(data: ByteBuffer) = Range( data.getShort(2).amps / 100
+                                            , data.getShort(0).amps / 100)
 }
 
 case class ReadM2CurrentLimit(address: Byte) extends Command {
   type ResponseType = Range[ElectricCurrent]
-  def bytes = Seq(address, 0x89.toByte)
-  def parseResults(data: Seq[Byte]) = Range( Utilities.readSignedShort(data.drop(2)).amps / 100
-                                           , Utilities.readSignedShort(data).amps / 100)
+  val command = 137.toByte
+  def parseResults(data: ByteBuffer) = Range( data.getShort(2).amps / 100
+                                            , data.getShort(0).amps / 100)
 }
 
 sealed trait PwmMode
 case object LOCKED_ANTIPHASE extends PwmMode
 case object SIGN_MAGNITUDE extends PwmMode
 
-case class SetPwmMode(address: Byte, mode: PwmMode) extends Command {
-  type ResponseType = Unit
-  def bytes = Utilities.withCrc(Seq(address, 0x94.toByte, if(mode == LOCKED_ANTIPHASE) 0x00 else 0x01))
-  def parseResults(data: Seq[Byte]) = Unit
+case class SetPwmMode(address: Byte, mode: PwmMode) extends UnitCommand {
+  val command = 148.toByte
+  override def populateByteBuffer(buf: ByteBuffer): Int = {
+    buf.put(0, address)
+    buf.put(1, command)
+    buf.put(2, if(mode == LOCKED_ANTIPHASE) 0 else 1)
+    addCrc(buf, 3)
+    8
+  }
 }
 
 case class ReadPwmMode(address: Byte) extends Command {
   type ResponseType = PwmMode
-  def bytes = Seq(address, 0x95.toByte)
-  def parseResults(data: Seq[Byte]) = if (data(0) == 0) LOCKED_ANTIPHASE else SIGN_MAGNITUDE
+  val command = 149.toByte
+  def parseResults(data: ByteBuffer) = if (data.get(0) == 0) LOCKED_ANTIPHASE else SIGN_MAGNITUDE
 }
+
+
+// ENCODER COMMANDS
+case class EncoderStatus(status: Byte) {
+  def underflow: Boolean = (status & 1) != 0
+  def overflow: Boolean = (status & 4) != 0
+  def forward: Boolean = (status & 2) != 0
+}
+
+case class ReadM1Encoder(address: Byte) extends Command {
+  type ResponseType = (Long, EncoderStatus)
+  val command = 16.toByte
+  def parseResults(data: ByteBuffer) = {
+    (data.getInt(0).toLong + Int.MaxValue, EncoderStatus(data.get(4)))
+  }
+}
+
+
 
 
--- src/main/scala/com/github/dunmatt/roboclaw/Utilities.scala
+++ src/main/scala/com/github/dunmatt/roboclaw/Utilities.scala
@@ -1,23 +1,14 @@
 package com.github.dunmatt.roboclaw
 
+import java.nio.ByteBuffer
+
 object Utilities {
-  def crc16(packet: Seq[Byte]): Char = {
-    packet.foldLeft(0.toChar) { case (crc, b) =>
-      (0 until 8).foldLeft((crc ^ b).toChar) {
-        case (c, _) if (c & 0x8000) > 0 => ((c << 1) ^ 0x1021).toChar
-        case (c, _) => (c << 1).toChar
+  def crc16(packet: ByteBuffer, dataLength: Int): Char = {
+    (0 until dataLength).foldLeft(0.toChar) { case (crc, i) =>
+      (0 until 8).foldLeft((crc ^ (packet.get(i) & 0xFF)).toChar) {
+        case (c, _) if (c & 0x8000) == 0 => (c << 1).toChar
+        case (c, _) => ((c << 1) ^ 0x1021).toChar
       }
     }
-  }
-
-  def withCrc(packet: Seq[Byte]): Seq[Byte] = {
-    val crc = crc16(packet)
-    packet :+ (crc >> 8).toByte :+ crc.toByte
-  }
-
-  def readSignedShort(bytes: Seq[Byte]): Short = ((bytes(0) << 8) | bytes(1)).toShort
-
-  def writeSignedShort(value: Double): Seq[Byte] = {
-    List((value.toShort >>> 8).toByte, value.toByte)
   }
 }
