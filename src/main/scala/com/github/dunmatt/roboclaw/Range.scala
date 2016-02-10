496b4cc0cd47b6526dba0300e46240b42ec7a30b
M@ <mattdunlap+github@gmail.com>
2016-02-09 20:02:51 -0500

    added contains to range

--- src/main/scala/com/github/dunmatt/roboclaw/Range.scala
+++ src/main/scala/com/github/dunmatt/roboclaw/Range.scala
@@ -1,3 +1,22 @@
 package com.github.dunmatt.roboclaw
 
-case class Range[D](min: D, max: D) {}
+import squants.Quantity
+
+trait Range[D] {
+  def min: D
+  def max: D
+  def contains(q: D): Boolean
+}
+
+case class IntRange(min: Int, max: Int) extends Range[Int] {
+  def contains(q: Int): Boolean = min <= q && q <= max
+}
+
+case class QuantityRange[D <: Quantity[D]](min: D, max: D) extends Range[D] {
+  def contains(q: D): Boolean = min <= q && q <= max
+}
+
+object Range {
+  def apply(min: Int, max: Int): Range[Int] = IntRange(min, max)
+  def apply[D <: Quantity[D]](min: D, max: D): Range[D] = QuantityRange(min, max)
+}
